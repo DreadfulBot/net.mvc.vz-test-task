acc37c7e4a5b3a0b2b873ca06ca1ec9279912da2
Friar-Tuck <toha_minin_555@list.ru>
2017-08-21 12:51:16 +0500

    commit №1. Base. Kinda.

---
+++ DBReport/Models/ReportInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//Отчёт по продажам
+namespace DBReport.Models
+{
+    public class ReportInfo
+    {
+        public int ProductId { get; set; }//Номер заказа
+        public DateTime Date { get; set; }//Дата заказа
+        public int Type { get; set; }//Артикул товара
+        public string Name { get; set; }//Название товара
+        public int Quantity { get; set; }//Кол-во реализованных единиц
+        public decimal Price { get; set; }//Цена реализации за единицу продукции
+    }
+}
